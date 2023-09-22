d617f8cb62eef773ee060cf785aced2c185fb9bf
Timon Scholzen <timon.scholzen@bib-buellingen.be>
2023-09-22 09:37:10 +0200

    Richtgen Code gmt

--- Sui.py
+++ Sui.py
@@ -1,4 +1,14 @@
-while True:
-    for i in range (1,100):
-        print(i)
-        print(i)
+print("Ungerade  |  Gerade")
+print("---------------------")
+
+for num in range(1, 21):
+    if num % 2 == 1:
+        
+        print(f"{num:^9} |", end=" ")
+    else:
+        # Gerade Zahl
+        print(f"{num:^7} |", end=" ")
+
+    if num % 2 == 0:
+        print()
+
